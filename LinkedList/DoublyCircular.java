d3498c84e31a8d42a5b90086f73454c4bfd64fa2
Sri-Varshini-A <srivarshinianandhu@gmail.com>
2025-09-27 13:26:49 +0530

    Create DoublyCircular.java

---
+++ LinkedList/DoublyCircular.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class DoublyCircular {
+    public static void main(String[] args) {
+      //System.out.println("Hello, World!");
+      Scanner sc = new Scanner(System.in);
+      ll obj = new ll();
+      while(sc.hasNext())
+      {
+        int d = sc.nextInt();
+        obj.ins(d);
+      }
+      obj.dis();
+  }
+}
+
+class Node
+{
+  int data;
+  Node left;
+  Node right;
+  Node(int d)
+  {
+    data = d;
+  }
+}
+
+class ll{
+  
+  Node head = null;
+  Node old = null;
+  
+  void ins(int val)
+  {
+    Node nn = new Node(val);
+    if(head==null)
+    {
+      head = nn;
+      old = nn;
+      nn.right = head;
+      head.left = nn;
+    }
+    else{
+      old.right = nn;
+      nn.left = old;
+      nn.right = head;
+      old = nn;
+      head.left = nn;
+    }
+  }
+  
+  void dis()
+  {
+    Node temp = head;
+    if(head==null)
+    {
+        return;
+    }
+    System.out.print(temp.data+" ");
+    temp = temp.right;
+    while(temp!=head)
+    {
+      System.out.print(temp.data+" ");
+      temp = temp.right;
+    }
+    
+  }
+}
